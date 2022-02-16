2de141af72bc30e544761e6267a087b285b3d392
yaroslav <yaroslav.grinevich@dsr-corporation.com>
2022-02-16 20:48:06 +0300

    [UI] Update VInput, add VPassword, VSearch, VPopup and VDropdown

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,45 +1,43 @@
 <template>
-  <VeeForm v-slot="{ handleSubmit }" :validation-schema="validation">
-    <form
-      class="login-form"
-      novalidate
-      autocomplete="off"
-      @submit.prevent="handleSubmit($event, submit)"
+  <form
+    class="login-form"
+    novalidate
+    autocomplete="off"
+    @submit.prevent="onSubmit"
+  >
+    <h1 class="login-form__header">
+      {{ i18n.t("welcome") }}
+    </h1>
+
+    <v-input
+      class="login-form__input"
+      label="Email"
+      name="email"
+      :invalid="!!emailError"
+      v-model="email"
     >
-      <h1 class="login-form__header">
-        {{ i18n.t("welcome") }}
-      </h1>
+      <template #error>
+        {{ emailError }}
+      </template>
+    </v-input>
 
-      <v-input
-        class="login-form__input"
-        label="Email"
-        name="email"
-        :invalid="validation.email"
-        v-model="credentials.email"
-      >
-        <template #error>
-          <VeeErrorMessage name="email" />
-        </template>
-      </v-input>
+    <v-password
+      class="login-form__input"
+      name="password"
+      :invalid="!!passwordError"
+      v-model="password"
+    >
+      <template #error>
+        {{ passwordError }}
+      </template>
+    </v-password>
 
-      <v-password
-        class="login-form__input"
-        name="password"
-        :invalid="validation.password"
-        v-model="credentials.password"
-      >
-        <template #error>
-          <VeeErrorMessage name="password" />
-        </template>
-      </v-password>
-
-      <div class="login-form__buttons">
-        <v-button type="submit" class="login-form__submit">{{
-          i18n.t("login")
-        }}</v-button>
-      </div>
-    </form>
-  </VeeForm>
+    <div class="login-form__buttons">
+      <v-button type="submit" class="login-form__submit">{{
+        i18n.t("login")
+      }}</v-button>
+    </div>
+  </form>
 </template>
 
 <script lang="ts">
@@ -47,11 +45,7 @@ import { defineComponent, PropType } from "vue";
 import VButton from "@/components/VButton.vue";
 import VInput from "@/components/VInput.vue";
 import { useI18n } from "vue-i18n";
-import {
-  Form as VeeForm,
-  ErrorMessage as VeeErrorMessage,
-  SubmissionContext,
-} from "vee-validate";
+import { useForm, useField } from "vee-validate";
 import * as yup from "yup";
 import { ServerError } from "@/types/serverError";
 import VPassword from "@/components/VPassword.vue";
@@ -62,8 +56,6 @@ export default defineComponent({
     VPassword,
     VButton,
     VInput,
-    VeeForm,
-    VeeErrorMessage,
   },
   props: {
     serverErrors: {
@@ -71,25 +63,36 @@ export default defineComponent({
     },
   },
   emits: ["login"],
-  setup() {
+  setup(_, { emit }) {
+    const validationSchema = yup.object({
+      email: yup.string().required().email(),
+      password: yup.string().required().min(4),
+    });
+
+    const { handleSubmit } = useForm({
+      validationSchema,
+      initialValues: {
+        email: "",
+        password: "",
+      },
+    });
+
+    const onSubmit = handleSubmit((values) => {
+      emit("login", values);
+    });
+
+    const { value: email, errorMessage: emailError } = useField("email");
+    const { value: password, errorMessage: passwordError } =
+      useField("password");
+
     return {
       i18n: useI18n(),
+      onSubmit,
+      email,
+      emailError,
+      password,
+      passwordError,
     };
-  },
-  data() {
-    return {
-      credentials: { email: "", password: "" },
-      validation: yup.object().shape({
-        email: yup.string().required(),
-        password: yup.string().required().min(4),
-      }),
-    };
-  },
-  methods: {
-    submit<H, C extends SubmissionContext>(value: H, actions: C) {
-      debugger;
-      this.$emit("login", this.credentials, actions.setFieldError);
-    },
   },
 });
 </script>
